9a0e6804ef22f650df34b573676acb1c724a27cc
Nahuel Cuesta Luengo <ncuesta@cespi.unlp.edu.ar>
2015-09-18 16:37:56 -0300

    Changes default value for :allow_html to true

--- app/inputs/select2_input.rb
+++ app/inputs/select2_input.rb
@@ -23,6 +23,7 @@ class Select2Input < SimpleForm::Inputs::Base
           s[:void_option] = options.delete(:void_option) if options[:void_option]
           s[:opts_for_select2] = options.delete(:opts_for_select2) if options[:opts_for_select2]
           s[:can_create_on_empty_result] = options.delete(:can_create_on_empty_result) if options[:can_create_on_empty_result]
+          s[:allow_html]  = true # Default value
           s[:allow_html]  = options.delete(:allow_html)  if options[:allow_html]
         end
 
